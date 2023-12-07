f356b2717f736d2ed7b2552d29b66d75a5030dda
allysonpacheco@yahoo.com <allysonpacheco@yahoo.com>
2023-12-06 19:49:43 -0500

    Edited BUY_COOKIE to ADD_COOKIE and REMOVE_COOKIE to DELETE_COOKIE in the respective pages files

--- client/src/pages/CheckoutCart.jsx
+++ client/src/pages/CheckoutCart.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 // Insert CSS types here
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_ME } from '../utils/queries';
-import { REMOVE_COOKIE } from '../utils/mutations';
+import { DELETE_COOKIE } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { removeCookieId } from '../utils/localStorage';
+import { deleteCookieId } from '../utils/localStorage';
