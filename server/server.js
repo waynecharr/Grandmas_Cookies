834b5bbc4ab73bd47b930834d54b9227f659b12d
waynecharr <137343274+waynecharr@users.noreply.github.com>
2023-12-10 17:28:59 -0500

    updated server.js to hopefully resolve GraphQL error

--- server/server.js
+++ server/server.js
@@ -1,60 +1,80 @@
+const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
-// import ApolloServer
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const { authMiddleware } = require('./utils/auth');
 
-// import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
 
-const PORT = process.env.PORT || 3011;
-// create a new Apollo server and pass in our schema data
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, world!',
+  },
+};
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
+  introspection: true,
 });
 
 const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+server.applyMiddleware({ app });
 
-const path = require('path');
-
-// Create a new instance of an Apollo server with the GraphQL schema
-const startServer = async (typeDefs, resolvers) => {
-  await server.start();
-
-  // integrate our Apollo server with the Express application as middleware
-  // server.applyMiddleware({ app });
-
-  // Serve up static assets
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist/')));
-
-  app.use('/graphql', expressMiddleware (server, {context: authMiddleware}));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  });
-  }
-
-  db.once('open', () => {
-    app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-      );
-    });
-  });
-};
-
-// Call the async function to start the server
-startServer().catch((error) => {
-  console.error('Error starting the server:', error);
+const PORT = process.env.PORT || 3024;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}/graphql`);
 });
 
+// const express = require('express');
+// const { ApolloServer } = require('@apollo/server');
+// const { expressMiddleware } = require('@apollo/server/express4');
+// const path = require('path');
+// const { authMiddleware } = require('./utils/auth');
+
+// const { typeDefs, resolvers } = require('./schemas');
+// const db = require('./config/connection');
+
+// const PORT = process.env.PORT || 3023;
+// const app = express();
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   introspection: true,
+// });
+
+// const startApolloServer = async () => {
+//   await server.start();
+  
+//   app.use(express.urlencoded({ extended: true }));
+//   app.use(express.json());
+  
+
+//   app.use('/graphql', expressMiddleware(server, {
+//     context: authMiddleware
+//   }));
+
+//   // Serve up static assets
+//   app.use('/images', express.static(path.join(__dirname, '../client/images')));
+
+//   if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../client/dist')));
+
+//     app.get('*', (req, res) => {
+//       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+//     });
+//   } 
+
+//   db.once('open', () => {
+//     app.listen(PORT, () => {
+//       console.log(`API server running on port ${PORT}!`);
+//       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+//     });
+//   });
+// };
+
+// startApolloServer();
 
 
 
