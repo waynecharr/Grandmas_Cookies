6b53aa797a8119c23550f281838b5c3de9e66830
DMB-LEB <nhnative2020@gmail.com>
2023-12-11 18:29:36 -0500

    Added Apollo to App.jsx and made updates to commented out code

--- client/src/components/LoginFormBoot.jsx
+++ client/src/components/LoginFormBoot.jsx
@@ -2,10 +2,10 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-// import { loginUser } from '../utils/API';
+import { loginUser } from '../utils/API';
 import AuthService from '../utils/auth';
 
-import { useMutation } from '@apollo/client';
+// import { useMutation } from '@apollo/client';
 
 
 const LoginFormBoot = () => {
